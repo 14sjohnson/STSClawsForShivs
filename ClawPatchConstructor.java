44ccd3c3fc0fc7c4c92c8288e2b9268d9a02032e
Scott <johnson.scott.14@gmail.com>
2023-01-06 22:19:07 -0700

    Claw magic number now 1, description code added in comments

--- ClawPatchConstructor.java
+++ ClawPatchConstructor.java
@@ -27,6 +27,9 @@ public class ClawPatchConstructor {
 
     public static void Postfix(Claw __instance) {
         __instance.baseDamage += ClawCountTracker.timesClawHasBeenUsed*2;
+        __instance.baseMagicNumber = 1;
+        __instance.magicNumber = 1;
+        //__instance.rawDescription = "Deal 2 damage, increase all claw's damage by 1";
         logger.info("ClawPatch: Here"+__instance.baseDamage);
     }
 }
