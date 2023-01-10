5fe5f8f188184c4d033ec77ed35dca38da052f4f
Scott <johnson.scott.14@gmail.com>
2023-01-09 21:47:25 -0700

    Cloak and dagger wasn't added last commit, and ClawPatch wasn't renamed I don't think

--- ClawPatch.java
+++ ClawPatch.java
@@ -4,32 +4,26 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-//
-//@SpirePatch(clz = Claw.class, method = "use")
-//public class ClawPatch {
-//    @SpireInsertPatch(rloc = 4)
-//    public static void Insert(Claw __instance) {
-//        AbstractDungeon.actionManager.addToBottom((AbstractGameAction)new GashAction(__instance, 10));
-//    }
-//}
-////
-////@SpirePatch(clz = Claw.class, method = )
-//
-//
-//
-//
+@SpirePatch(clz = Claw.class, method = "use")
+class ClawPatch {
+    public static final Logger logger = LogManager.getLogger(ModInitializer.class.getName());
 
+
+    public static void Postfix(Claw __instance) {
+        ClawCountTracker.increaseClawCount();
+    }
+}
 
 @SpirePatch(clz = Claw.class, method = SpirePatch.CONSTRUCTOR)
-public class ClawPatchConstructor {
+class ClawPatchCon {
     public static final Logger logger = LogManager.getLogger(ModInitializer.class.getName());
 
 
     public static void Postfix(Claw __instance) {
         __instance.baseDamage += ClawCountTracker.timesClawHasBeenUsed*2;
-        __instance.baseMagicNumber = 1;
-        __instance.magicNumber = 1;
+        //__instance.baseMagicNumber = 1;
+        //__instance.magicNumber = 1;
         //__instance.rawDescription = "Deal 2 damage, increase all claw's damage by 1";
-        logger.info("ClawPatch: Here"+__instance.baseDamage);
+        //logger.info("ClawPatch: Here"+__instance.baseDamage);
     }
 }
